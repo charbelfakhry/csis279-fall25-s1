e44b039b1f40c4d3ac81102322eb1beaf43a75c5
Georgio Mazraani <mazraanigeorgio@gmail.com>
2024-09-24 01:11:42 +0300

    models and services part 1

---
+++ api/models/users.js
@@ -0,0 +1,54 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/configSqlz");
+
+const User = sequelize.define('User', {
+    user_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+
+    user_username: {
+        type: DataTypes.STRING(50),
+        allowNull: true,
+    },
+
+    user_pass: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+
+    user_email: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        unique: true,
+    },
+
+    user_phone: {
+        type: DataTypes.BIGINT,
+        allowNull: true,
+        unique: true,
+    },
+
+    joined_on: {
+        type: DataTypes.DATE,
+        allowNull: true,
+    },
+
+}, {
+    tableName: "users",
+    timestamps: false,
+});
+
+User.associate = (models) => {
+    User.hasMany(models.Order, {
+        foreignKey: 'user_id',
+        as: 'orders',
+    });
+
+    User.hasMany(models.Review, {
+        foreignKey: 'user_id',
+        as: 'reviews',
+    });
+};
+module.exports = User;
