2fb1417434e9d7f1adecfe43fa91ae4b6c3aad2a
Faysoula <faysaljunior.elestwani@gmail.com>
2024-09-25 22:33:02 +0300

    Refactor authentication middleware to handle missing token and invalid token errors

--- api/controllers/authenticateUserController.js
+++ api/controllers/authenticateUserController.js
@@ -6,12 +6,16 @@ require("dotenv").config();
 const authenticateUserController = async (req, res) => {
   const { email, pass } = req.body;
 
+  if(!email || !pass) {
+    return res.status(400).json({ message: "missing email or pass" });
+  }
+
   try {
     const user = await User.findOne({ where: { user_email: email } });
     if (!user) {
       return res.status(404).json({ message: "invalid email or pass" });
     }
-    const validPass = await bcrypt.compare(pass, user.pass);
+    const validPass = await bcrypt.compare(pass, user.pass);//salted pass
     if (!validPass) {
       return res.status(404).json({ message: "invalid email or pass" });
     }
