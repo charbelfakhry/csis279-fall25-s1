852708db02d3049399b63bbb3cb9b14e8d3dac98
Faysoula <faysaljunior.elestwani@gmail.com>
2024-09-30 20:40:18 +0300

    added index.js file

---
+++ api/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const userRoutes = require("./Routes/usersRoutes");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("api running yay");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`====================================`);
+  console.log(`Server running on port ${PORT}`);
+  console.log(`====================================`);
+});
