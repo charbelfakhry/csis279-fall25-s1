2cd719fda03f3513309c3de131c2119237d04ff8
Faysoula <faysaljunior.elestwani@gmail.com>
2024-10-08 22:58:51 +0300

    added 'add user' form and tested

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-import React from "react"
-import Message from './components/Message';
-import ListGroup from './components/ListGroup';
-import Users from './components/Users';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import Message from "./components/Message";
+import ListGroup from "./components/ListGroup";
+import Users from "./components/Users";
+import CreateUser from "./components/CreateUser";
 
 function App() {
   return (
-    <>
-      <Users />
-    </>
+    <div className="container">
+      <h2>user managment!!yay</h2>
+
+      <div className="mt-5">
+        <h2>create usher here</h2>
+        <CreateUser />
+      </div>
+
+      <div className="mt-5">
+        <h2>existing users</h2>
+        <Users />
+      </div>
+
+    </div>
   );
 }
 
